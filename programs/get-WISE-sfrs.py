4b6a65efe589be27f78f7f58d7ad1c75dfe8f10c
rfinn@siena.edu <rfinn@siena.edu>
2018-06-07 15:57:15 -0400

    updates

--- programs/get-WISE-sfrs.py
+++ programs/get-WISE-sfrs.py
@@ -1,4 +1,5 @@
 from astropy.io import fits
+from astropy.table import Table
 import sys
 sys.path.append('./CharyElbaz/')
 import chary_elbaz_24um as chary
@@ -35,17 +36,22 @@ w4_flux_Jy[wflag] = F0*10**(-1*wise.W4MPRO[wflag]/2.5)
 
 ce_lir[wflag],ce_sfr[wflag]=chary.chary_elbaz_24um(nsa.Z[wflag],w4_flux_Jy[wflag]*1.e6)
 
-# write out sfr and Lir to a fits table
-wdat = fits.open(catpath + 'WISE_virgo.fits')[1].data
-sfrcol = np.zeros(len(wdat))
-sfrcol[wflag] = ce_sfr[wflag]
-sfrcol_new = fits.Column(name = 'SFR', format = 'D', array = sfrcol)
+## # write out sfr and Lir to a fits table
+## wdat = fits.open(catpath + 'WISE_virgo.fits')[1].data
+## sfrcol = np.zeros(len(wdat))
+## sfrcol[wflag] = ce_sfr[wflag]
+## sfrcol_new = fits.Column(name = 'SFR', format = 'D', array = sfrcol)
 
-lircol = np.zeros(len(wdat))
-lircol[wflag] = ce_lir[wflag]
-lircol_new = fits.Column(name = 'Lir', format = 'D', array = lircol)
+## lircol = np.zeros(len(wdat))
+## lircol[wflag] = ce_lir[wflag]
+## lircol_new = fits.Column(name = 'Lir', format = 'D', array = lircol)
 
-origcols = wdat.columns
-hdu = fits.BinTableHDU.from_columns(origcols + sfrcol_new + lircol_new)
-outfile = catpath + 'WISE_virgo.fits'
-hdu.writeto(outfile, clobber = 'True')
+## origcols = wdat.columns
+## hdu = fits.BinTableHDU.from_columns(origcols + sfrcol_new + lircol_new)
+## outfile = catpath + 'WISE_virgo.fits'
+## hdu.writeto(outfile, clobber = 'True')
+
+cols = [nsa.NSAID,nsa.RA,nsa.DEC,ce_lir,ce_sfr]
+colnames = ['NSAID','RA','DEC','L_IR','SFR_IR']
+newtable = Table(cols, names=colnames)
+newtable.write(gitpath+'tables/nsa_wise_sfrs.virgo.fits',overwrite=True,format='fits')
