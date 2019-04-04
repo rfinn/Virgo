d321084a51a9c0538b9a1243ce017b0c93677499
rfinn@siena.edu <rfinn@siena.edu>
2019-04-04 12:24:15 -0400

    updates

--- programs/virgoCommon.py
+++ programs/virgoCommon.py
@@ -3,14 +3,14 @@ import os
 #Get current path so program can tell if this is being run on Kelly's or Rose's computer
 mypath=os.getcwd()
 if mypath.find('rfinn') > -1:
-    print "Running on Rose's computer"
+    print("Running on Rose's computer")
     #agcfile='/Users/rfinn/idl/programs/idl_alfa/agctotal.sav'
     gitpath='/Users/rfinn/github/'
     nsapath = '/Users/rfinn/research/NSA/'
     gswlpath = '/Users/rfinn/Dropbox/Research/GSWLC/'
     agcpath = '/Users/rfinn/research/AGC/'
 elif mypath.find('kelly') > -1:
-    print "Running on Kellys's computer"
+    print("Running on Kellys's computer")
     gitpath='/Users/kellywhalen/Github/'
     nsapath = '/Users/kellywhalen/RESEARCH/NSA_table/'
     gswlpath = '/Users/kellywhalen/RESEARCH/GSWLC/'
