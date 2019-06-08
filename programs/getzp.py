33a3824d737c668882888bedc380af9bf3b15ac0
rfinn@siena.edu <rfinn@siena.edu>
2019-06-08 12:12:47 -0400

    converted to class

--- programs/getzp.py
+++ programs/getzp.py
@@ -107,201 +107,203 @@ def panstarrs_query(ra_deg, dec_deg, rad_deg, maxmag=20,
                                catalog="II/349/ps1")[0]
 
 
+class getzp():
+    def __init__(self, image, instrument='h', filter='r', astromatic_dir = '~/github/HalphaImaging/astromatic/')
+
+        self.image = image
+        self.astrodir = astromatic_dir
+        self.instrument = instrument
+        self.filter = filter
+    def getzp(self):
+        self.runse()
+        self.get_panstarrs()
+        self.match_coords()
+        self.fitzp()
+    def runse(self):
+        #####
+        # Run Source Extractor on image to measure magnitudes
+        ####
+
+        os.system('cp ' +args.d + '/default.* .')
+        t = args.image.split('.fits')
+        froot = t[0]
+        if args.instrument == 'h':
+            os.system('sex ' + args.image + ' -c default.sex.HDI -CATALOG_NAME ' + froot + '.cat -MAG_ZEROPOINT 0')
+
+        elif args.instrument == 'i':
+            os.system('sex ' + args.image + ' -c default.sex.INT -CATALOG_NAME ' + froot + '.cat -MAG_ZEROPOINT 0')
+
+        elif args.instrument == 'm':
+            os.system('sex ' + args.image + ' -c default.sex.HDI -CATALOG_NAME ' + froot + '.cat -MAG_ZEROPOINT 0')
+
+        # clean up SE files
+        # skipping for now in case the following command accidentally deletes user files
+        # os.system('rm default.* .')
 
 
-parser = argparse.ArgumentParser(description ='Run sextractor, get Pan-STARRS catalog, and then computer photometric ZP\n \n from within ipython: \n %run ~/github/Virgo/programs/getzp.py --image pointing031-r.coadd.fits --instrument i \n\n then:\n x,y = fitzp() \n \n The y intercept is -1*ZP. \n \n x and y data are returned in case you want to make additional plots.', formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('--image', dest = 'image', default = None, help = 'Image for ZP calibration')
-parser.add_argument('--instrument', dest = 'instrument', default = None, help = 'HDI = h, KPNO mosaic = m, INT = i')
-parser.add_argument('--nsigma', dest = 'nsigma', default = 2.0, help = 'number of std to use in iterative rejection of ZP fitting')
-parser.add_argument('--d',dest = 'd', default ='~/github/HalphaImaging/astromatic', help = 'Locates path of default config files.  Default is ~/github/HalphaImaging/astromatic')
-args = parser.parse_args()
+        ###################################
+        # Read in Source Extractor catalog
+        ###################################
+
+        secat_filename = froot+'.cat'
+        self.secat = fits.getdata(secat_filename,2)
+
+        ###################################
+        # get max/min RA and DEC for the image
+        ###################################
+
+        minRA = min(secat['ALPHA_J2000'])
+        maxRA = max(secat['ALPHA_J2000'])
+        minDEC = min(secat['DELTA_J2000'])
+        maxDEC = max(secat['DELTA_J2000'])
+        self.centerRA = 0.5*(minRA + maxRA)
+        self.centerDEC = 0.5*(minDEC + maxDEC)
+        #radius = np.sqrt((maxRA- centerRA)**2 + (maxDEC - centerDEC)**2)
+        #print(radius)
+
+        # NOTE - radius is with width of the rectangular
+        # search area.  This is not a circular search, as I orginally thought.
+        self.radius = max((maxRA - minRA), (maxDEC - minDEC))
+    def get_panstarrs(self):
+
+        ###################################
+        # get Pan-STARRS catalog over the same region
+        ###################################
+
+        self.pan = panstarrs_query(self.centerRA, self.centerDEC, self.radius)
+    def match_coords(self):
+        ###################################
+        # match Pan-STARRS1 data to Source Extractor sources
+        # remove any objects that are saturated or non-linear in our r-band image
+        ###################################
+
+        pancoords = SkyCoord(self.pan['RAJ2000'],self.pan['DEJ2000'],frame='icrs')
+        secoords = SkyCoord(self.secat['ALPHA_J2000']*u.degree,self.secat['DELTA_J2000']*u.degree,frame='icrs')
+
+        index,dist2d,dist3d = pancoords.match_to_catalog_sky(secoords)
+
+        # only keep matches with matched RA and Dec w/in 5 arcsec
+        matchflag = dist2d.degree < 5./3600
 
 
+        self.matchedarray1=np.zeros(len(pancoords),dtype=secat.dtype)
+        self.matchedarray1[matchflag] = self.secat[index[matchflag]]
+
+        ###################################
+        # remove any objects that are saturated, have FLAGS set, galaxies, must have 14 < r < 17 according to Pan-STARRS
+        ###################################
 
 
-#####
-# Run Source Extractor on image to measure magnitudes
-####
+        fitflag = matchflag & (matchedarray1['FLAGS'] == 0)  & (pan['rmag'] > 14.) & (pan['rmag'] < 17.) & (matchedarray1['CLASS_STAR'] > 0.95)
 
-os.system('cp ' +args.d + '/default.* .')
-t = args.image.split('.fits')
-froot = t[0]
-if args.instrument == 'h':
-    os.system('sex ' + args.image + ' -c default.sex.HDI -CATALOG_NAME ' + froot + '.cat -MAG_ZEROPOINT 0')
+        if self.filter == 'R':
+            ###################################
+            # Calculate Johnson R
+            ###################################
+            self.R = pan['rmag'] + (-0.153)*(pan['rmag']-pan['imag']) - 0.117
+        else:
+            self.R = pan['rmag']
+        ###################################
+        # Show location of residuals
+        ###################################
 
-elif args.instrument == 'i':
-    os.system('sex ' + args.image + ' -c default.sex.INT -CATALOG_NAME ' + froot + '.cat -MAG_ZEROPOINT 0')
+    def fitzp(plotall=False):
+        ###################################
+        # Solve for the zeropoint
+        ###################################
 
-elif args.instrument == 'm':
-    os.system('sex ' + args.image + ' -c default.sex.HDI -CATALOG_NAME ' + froot + '.cat -MAG_ZEROPOINT 0')
-
-# clean up SE files
-# skipping for now in case the following command accidentally deletes user files
-# os.system('rm default.* .')
-
-
-###################################
-# Read in Source Extractor catalog
-###################################
-
-secat_filename = froot+'.cat'
-secat = fits.getdata(secat_filename,2)
-
-###################################
-# get max/min RA and DEC for the image
-###################################
-
-minRA = min(secat['ALPHA_J2000'])
-maxRA = max(secat['ALPHA_J2000'])
-minDEC = min(secat['DELTA_J2000'])
-maxDEC = max(secat['DELTA_J2000'])
-centerRA = 0.5*(minRA + maxRA)
-centerDEC = 0.5*(minDEC + maxDEC)
-#radius = np.sqrt((maxRA- centerRA)**2 + (maxDEC - centerDEC)**2)
-#print(radius)
-
-# NOTE - radius is with width of the rectangular
-# search area.  This is not a circular search, as I orginally thought.
-radius = max((maxRA - minRA), (maxDEC - minDEC))
-
-
-###################################
-# get Pan-STARRS catalog over the same region
-###################################
-
-pan = panstarrs_query(centerRA, centerDEC, radius)
-
-###################################
-# match Pan-STARRS1 data to Source Extractor sources
-# remove any objects that are saturated or non-linear in our r-band image
-###################################
-
-pancoords = SkyCoord(pan['RAJ2000'],pan['DEJ2000'],frame='icrs')
-secoords = SkyCoord(secat['ALPHA_J2000']*u.degree,secat['DELTA_J2000']*u.degree,frame='icrs')
-
-index,dist2d,dist3d = pancoords.match_to_catalog_sky(secoords)
-
-# only keep matches with matched RA and Dec w/in 5 arcsec
-matchflag = dist2d.degree < 5./3600
-
-
-matchedarray1=np.zeros(len(pancoords),dtype=secat.dtype)
-matchedarray1[matchflag] = secat[index[matchflag]]
-
-###################################
-# remove any objects that are saturated, have FLAGS set, galaxies, must have 14 < r < 17 according to Pan-STARRS
-###################################
-
-
-fitflag = matchflag & (matchedarray1['FLAGS'] == 0)  & (pan['rmag'] > 14.) & (pan['rmag'] < 17.) & (matchedarray1['CLASS_STAR'] > 0.95)
-
-###################################
-# Calculate Johnson R
-###################################
-R = pan['rmag'] + (-0.153)*(pan['rmag']-pan['imag']) - 0.117
-
-
-###################################
-# Show location of residuals
-###################################
-
-
-
-###################################
-# Solve for the zeropoint
-###################################
-
-# plot Pan-STARRS r mag on x axis, observed R-mag on y axis
-
-
-def fitzp(plotall=False):
-    plt.figure(figsize=(8,6))
-    flag = fitflag
-    plt.plot(pan['rmag'][flag],matchedarray1['MAG_AUTO'][flag],'bo')
-    plt.errorbar(pan['rmag'][flag],matchedarray1['MAG_AUTO'][flag],xerr= pan['e_rmag'][fitflag],yerr=matchedarray1['MAGERR_AUTO'][fitflag],fmt='none')
-    plt.plot(pan['rmag'][flag],matchedarray1['MAG_BEST'][flag],'ro',label='MAG_BEST')
-    plt.plot(pan['rmag'][flag],matchedarray1['MAG_PETRO'][flag],'go',label='MAG_PETRO')
-    plt.plot(pan['rmag'][flag],matchedarray1['MAG_ISO'][flag],'ko',label='MAG_ISO')
-    plt.xlabel('Pan-STARRS r',fontsize=16)
-    plt.ylabel('SE R-band MAG_AUTO',fontsize=16)
-    c = np.polyfit(pan['rmag'][flag],matchedarray1['MAG_AUTO'][flag],1)
-    xl = np.linspace(14,17,10)
-    yl = np.polyval(c,xl)
-    plt.plot(xl,yl,'k--')
-    #plt.plot(xl,1.2*yl,'k:')
-    #print(c)
-    
-    yfit = np.polyval(c,pan['rmag'])
-    residual = np.zeros(len(fitflag))
-    residual[flag] = (yfit[flag] - matchedarray1['MAG_AUTO'][flag])/yfit[flag]
-    bestc = np.array([0,0],'f')
-    plt.figure()
-    plt.scatter(matchedarray1['X_IMAGE'][flag],matchedarray1['Y_IMAGE'][flag],c = abs(residual[flag]))
-    plt.colorbar()
-    delta = 100.
-    
-    x = pan['rmag'][fitflag]
-    x = R[fitflag]
-    y = matchedarray1['MAG_AUTO'][fitflag]
-    while delta > 1.e-3:
-        c = np.polyfit(x,y,1)
-        print('number of points retained = ',sum(flag))
-        yfit = np.polyval(c,x)
-        residual = (yfit - y)/yfit 
-
+        # plot Pan-STARRS r mag on x axis, observed R-mag on y axis
+        flag = self.fitflag
+        c = np.polyfit(self.pan['rmag'][flag],self.matchedarray1['MAG_AUTO'][flag],1)
         if plotall:
-            plt.figure(figsize=(8,8))
-            plt.subplot(2,1,1)
-            plt.plot(x,y,'bo',label='MAG_AUTO')
+            plt.figure(figsize=(8,6))
+
+            plt.plot(self.pan['rmag'][flag],self.matchedarray1['MAG_AUTO'][flag],'bo')
+            plt.errorbar(self.pan['rmag'][flag],self.matchedarray1['MAG_AUTO'][flag],xerr= pan['e_rmag'][flag],yerr=matchedarray1['MAGERR_AUTO'][flag],fmt='none')
+            plt.plot(self.pan['rmag'][flag],self.matchedarray1['MAG_BEST'][flag],'ro',label='MAG_BEST')
+            plt.plot(self.pan['rmag'][flag],self.matchedarray1['MAG_PETRO'][flag],'go',label='MAG_PETRO')
+            plt.plot(self.pan['rmag'][flag],self.matchedarray1['MAG_ISO'][flag],'ko',label='MAG_ISO')
             plt.xlabel('Pan-STARRS r',fontsize=16)
-            plt.ylabel('SE R-band MAG',fontsize=16)
+            plt.ylabel('SE R-band MAG_AUTO',fontsize=16)
 
             xl = np.linspace(14,17,10)
             yl = np.polyval(c,xl)
-            s = 'fit: y = %.2f PAN + %.2f'%(c[0],c[1])
-            plt.plot(xl,yl,'k--',label=s)
-            plt.legend()
-
-
-            plt.subplot(2,1,2)
-            s = 'std = %.4f'%(np.std(residual))
-            plt.plot(x,residual, 'ko',label=s)
-            plt.xlabel('Pan-STARRS r',fontsize=16)
-            plt.ylabel('YFIT - SE R-band MAG_AUTO',fontsize=16)
-            plt.legend()
-            plt.axhline(y=0,color='r')
+            plt.plot(xl,yl,'k--')
+            #plt.plot(xl,1.2*yl,'k:')
+            #print(c)
     
-        # check for convergence
-        print(bestc[1],c[1])
-        delta = abs(bestc[1] - c[1])
-        bestc = c
-        flag =  (abs(residual) < 2.0*np.std(residual))
-        x = x[flag]
-        y = y[flag]
-    # plot best-fit results
-    yfit = np.polyval(bestc,x)
-    residual = (yfit - y)/yfit 
-    plt.figure(figsize=(8,8))
-    plt.subplot(2,1,1)
-    plt.plot(x,y,'bo',label='MAG_AUTO')
-    plt.xlabel('Pan-STARRS r',fontsize=16)
-    plt.ylabel('SE R-band MAG',fontsize=16)
+        yfit = np.polyval(c,pan['rmag'])
+        residual = np.zeros(len(flag))
+        residual[flag] = (yfit[flag] - self.matchedarray1['MAG_AUTO'][flag])/yfit[flag]
+        self.bestc = np.array([0,0],'f')
+        
+        plt.figure()
+        plt.scatter(self.matchedarray1['X_IMAGE'][flag],matchedarray1['Y_IMAGE'][flag],c = abs(residual[flag]))
+        plt.colorbar()
+        delta = 100.
+        
+        #self.x = pan['rmag'][fitflag]
+        self.x = self.R[flag]
+        self.y = self.matchedarray1['MAG_AUTO'][flag]
+        while delta > 1.e-3:
+            c = np.polyfit(self.x,self.y,1)
+            print('number of points retained = ',sum(flag))
+            yfit = np.polyval(c,x)
+            self.residual = (yfit - y)/yfit 
+            if plotall:
+                self.plot_fitresults(polyfit_results = c)
+    
+            # check for convergence
+            print(bestc[1],c[1])
+            delta = abs(bestc[1] - c[1])
+            self.bestc = c
+            flag =  (abs(residual) < 2.0*np.std(residual))
+            self.x = x[flag]
+            self.y = y[flag]
+        self.plot_fitresults()
+    def plot_fitresults(self, polyfit_results = self.bestc):
+        # plot best-fit results
+        yfit = np.polyval(polyfit_results,self.x)
+        residual = (yfit - self.y)/yfit 
+        plt.figure(figsize=(8,8))
 
-    xl = np.linspace(14,17,10)
-    yl = np.polyval(c,xl)
-    s = 'fit: y = %.2f PAN + %.2f'%(c[0],c[1])
-    plt.plot(xl,yl,'k--',label=s)
-    plt.legend()
+        plt.subplot(2,1,1)
+        plt.plot(x,y,'bo',label='MAG_AUTO')
+        plt.xlabel('Pan-STARRS r',fontsize=16)
+        plt.ylabel('SE R-band MAG',fontsize=16)
+        xl = np.linspace(14,17,10)
+        yl = np.polyval(self.polyfit_results,xl)
+        s = 'fit: y = %.2f PAN + %.2f'%(polyfit_results[0],polyfit_results[1])
+        plt.plot(xl,yl,'k--',label=s)
+        plt.legend()
+        
+        plt.subplot(2,1,2)
+        s = 'std = %.4f'%(np.std(residual))
+        plt.plot(self.x,residual, 'ko',label=s)
+        plt.xlabel('Pan-STARRS r',fontsize=16)
+        plt.ylabel('YFIT - SE R-band MAG_AUTO',fontsize=16)
+        plt.legend()
+        plt.axhline(y=0,color='r')
+        
+    def update_header(self):
+        print('working on this')
+        # add best-fit ZP to image header
 
 
-    plt.subplot(2,1,2)
-    s = 'std = %.4f'%(np.std(residual))
-    plt.plot(x,residual, 'ko',label=s)
-    plt.xlabel('Pan-STARRS r',fontsize=16)
-    plt.ylabel('YFIT - SE R-band MAG_AUTO',fontsize=16)
-    plt.legend()
-    plt.axhline(y=0,color='r')
 
-    return x,y
+if __name__ = '__main__':
+
+
+    parser = argparse.ArgumentParser(description ='Run sextractor, get Pan-STARRS catalog, and then computer photometric ZP\n \n from within ipython: \n %run ~/github/Virgo/programs/getzp.py --image pointing031-r.coadd.fits --instrument i \n\n then:\n x,y = fitzp() \n \n The y intercept is -1*ZP. \n \n x and y data are returned in case you want to make additional plots.', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--image', dest = 'image', default = None, help = 'Image for ZP calibration')
+    parser.add_argument('--instrument', dest = 'instrument', default = None, help = 'HDI = h, KPNO mosaic = m, INT = i')
+    parser.add_argument('--filter', dest = 'filter', default = 'R', help = 'filter (R or r; use r for Halpha)')
+    parser.add_argument('--nsigma', dest = 'nsigma', default = 2.0, help = 'number of std to use in iterative rejection of ZP fitting')
+    parser.add_argument('--d',dest = 'd', default ='~/github/HalphaImaging/astromatic', help = 'Locates path of default config files.  Default is ~/github/HalphaImaging/astromatic')
+    args = parser.parse_args()
+
+    zp = getzp(args.image, intrument=args.instrument, filter=args.filter, astromatric_dir = args.d)
+    zp.getzp()
+
 
 
