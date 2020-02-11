8397864625ae77e22472579b001ebaad7fa1fa4c
rfinn@siena.edu <rfinn@siena.edu>
2020-02-11 18:28:43 -0500

    INT trauma

--- programs/reduce_INT_WFC.py
+++ programs/reduce_INT_WFC.py
@@ -2,50 +2,50 @@
 
 '''
 GOAL:
-- sort INT WFC data according to target and filter
+- Call theli scripts from the command line
+
+USAGE:
+- run from top directory for a particular night, after you have run sort_INT_WFC.py
 -
 
-RELEVANT HEADER KEYWORDS:
-
-    OBSTYPE: TARGET, SKY (skyflat), BIAS, FOCUS
-
-    IMAGETYP: object, sky, focus, zero
-
-    OBJECT: name that we assigned, i.e. pointing-021
-    - need to strip spaces
-
-    CATNAME:  similar to OBJECT, but capitalized? not all are the same.  use OBJECT
-
-    WFFBAND - filter; need to strip spaces
-
-    WFFPOS - filter position (can use this to double check)
-
-PROCEDURE:
-
-get list of filter, imagetype, and object names
-
-for flats, create a directory for each filter called FLAT-HALPHA.  Move flat to appropriate directory
-
-for bias, create BIAS, and move files there
-
-for focus, create FOCUS directory, move files there
-
-for science frames, sort them by filter into, e.g. SCIENCE-r or SCIENCE-Halpha
-- these will get sorted by object once bias subtraction and flatfielding is done.
-
-After basic calibration, we can sort science objects, make directory for each unique set of OBJECT-WFFBAND.  move files to appropriate directory.
 
 '''
 #import ccdproc
 from ccdproc import ImageFileCollection
 import os
+import stat
 import sys
+import subprocess
+
+
+###  SET ENVIRONMENT VARIABLES
+
+
+os.environ["INSTRUMENT"] = "WFC@INT"
+
 
 ##### SET PATHS  ####
 homedir = os.getenv("HOME")
-working_dir = os.getcwd()
+data_dir = os.getcwd()
 # path to theli scripts
-theli_path = homedir+'/theli/scripts/Linux_64/'
+if data_dir.find('Users/rfinn') > -1: # running on macbook
+    theli_path = homedir+'/software/theli/gui-2.10.5/scripts/'
+    # make sure python 2 is called instead of python 3
+    p = os.getenv("PATH")
+    os.environ["PATH"] = '/usr/bin/:'+p
+
+else: # assume this is Virgo machine
+    theli_path = homedir+'/gui-2.10.5/scripts/'
+    # make sure python 2 is called instead of python 3
+    p = os.getenv("PATH")
+    os.environ["PATH"] = '/usr/local/anaconda2/bin/:'+p
+
+
+# append path to scripts so it knows where they are
+p = os.getenv("PATH")
+os.environ["PATH"] = p+':'+theli_path
+
+os.environ["INSTRUMENT"] = "WFC@INT"
 
 
 # get list of directory names
@@ -58,41 +58,89 @@ for d in t:
 
 # start reduction script
 
-# split files
-for d in dirnames:
-    if os.path.exists(d):
-        command_string = theli_path+'process_split_WFC@INT.sh '+working_dir+' '+d
-        os.system(command_string)
+def split_files():
+    # change to theli dir because all scripts call other programs in a relative way
 
+    # split files
+    i=0
+    for d in dirnames:
+        if os.path.exists(d):
+            print('splitting files in ',d)
+            os.chdir(theli_path)
+            #os.chmod('process_split_WFC@INT.sh', stat.S_IEXEC)
+            command_string = './process_split_WFC@INT.sh '+data_dir+' '+d
+            rc = subprocess.call(command_string, shell=True)
+            #os.system(command_string)
+            os.chdir(data_dir)
+            i += 1
+            if i > 0:
+                return
+def process_bias():
+    # process BIAS frames
+    for d in dirnames:
+        if os.path.exists(d):
+            if d == 'BIAS':
+                # process bias frames
+                print('processing bias frames in', d)
+                command_string = theli_path+'parallel_manager.sh process_bias_para.sh '+data_dir+' BIAS'
+                os.system(command_string)
+def process_flats():
+    # process Flat frames
+    for d in dirnames:
+        if os.path.exists(d):
+            if d.find('FLAT'):
+                # process flats
+                print('processing flatfield images in ',d)
+                command_string = theli_path+'parallel_manager.sh process_flat_para.sh '+data_dir+' BIAS '+d
+                command_string = theli_path+'create_flat_ratio.sh '+data_dir+' '+d
+                command_string = theli_path+'parallel_manager.sh create_norm_para.sh '+data_dir+' '+d
 
-# process BIAS frames
-for d in dirnames:
-    if os.path.exists(d):
-        if d == 'BIAS':
-            # process bias frames
-            command_string = theli_path+'parallel_manager.sh process_bias_para.sh '+working_dir+' BIAS'
-            os.system(command_string)
-            
-# process Flat frames
-for d in dirnames:
-    if os.path.exists(d):
+def calibrate_images():
+    # calibrate images
+    # need to apply the correct flat to each image
+
+    flat_images = []
+    
+    for d in dirnames:
         if d.find('FLAT'):
-            # process flats
-            command_string = theli_path+'parallel_manager.sh process_flat_para.sh '+working_dir+' BIAS '+d
-            command_string = theli_path+'create_flat_ratio.sh '+working_dir+' '+d
-            command_string = theli_path+'parallel_manager.sh create_norm_para.sh '+working_dir+' '+d
+            flat_images.append(d)
 
-# calibrate images
-# need to apply the correct flat to each image
+    for f in flat_images:
+        ffilter = f.split('-')[1]
+        for d in dirnames:
+            #skip bias and flat images
+            if d.find('BIAS') > -1:
+                continue
+            if d.find('FLAT') > -1:
+                continue
+            if os.path.exists(d):
+                ofilter = d.split('-')[1]
+                if ofilter == ffilter:
+                    print('processing images in ',d)
+                    command_string =  theli_path+'/parallel_manager.sh process_science_para.sh '+data_dir+' BIAS '+f+' '+d
+                    os.system(command_string)
 
-flat_images = []
+def preview_weights():
+    # create a preview image
+    # calibrate weights
+    for d in dirnames:#skip bias and flat images
+        if d.find('BIAS') > -1:
+            continue
+        if d.find('FLAT') > -1:
+            continue
+        if os.path.exists(d):
+            print('creating preview and calibrating weights for ',d)
+            ofilter = d.split('-')[1]
+            command_string = theli_path+'make_album_WFC@INT.sh '+data_dir+' '+d+' OFC'
+            os.system(command_string)
+            command_string = theli_path+'create_tiff.sh '+data_dir+' '+d+' OFC'
+            os.system(command_string)
+            command_string = theli_path+'parallel_manager.sh create_global_weights_para.sh '+data_dir+' SKYFLAT-'+str(ofilter)+'_norm '+d 
+            os.system(command_string)
 
-for d in dirnames:
-    if d.find('FLAT'):
-        flat_images.append(d)
-
-for f in flat_images:
-    ffilter = f.split('-')[1]
+def create_weights():
+    
+    # create weights
     for d in dirnames:
         #skip bias and flat images
         if d.find('BIAS') > -1:
@@ -100,39 +148,31 @@ for f in flat_images:
         if d.find('FLAT') > -1:
             continue
         if os.path.exists(d):
-            ofilter = d.split('-')[1]
-            if ofilter == ffilter:
-                command_string =  theli_path+'/parallel_manager.sh process_science_para.sh '+working_dir+' BIAS '+f+' '+d
-                os.system(command_string)
-
-# create a preview image
-# calibrate weights
-for d in dirnames:
-        #skip bias and flat images
-        if d.find('BIAS') > -1:
-            continue
-        if d.find('FLAT') > -1:
-            continue
-        if os.path.exists(d):
-            ofilter = d.split('-')[1]
-            command_string = theli_path+'make_album_WFC@INT.sh '+working_dir+' '+d+' OFC'
+            print('creating weights for ',d)
+            command_string = theli_path+'transform_ds9_reg.sh '+data_dir+' '+d
             os.system(command_string)
-            command_string = theli_path+'create_tiff.sh '+working_dir+' '+d+' OFC'
-            os.system(command_string)
-            command_string = theli_path+'parallel_manager.sh create_global_weights_para.sh '+working_dir+' SKYFLAT-'+str(ofilter)+'_norm '+d 
+            command_string = theli_path+'parallel_manager.sh create_weights_para.sh '+data_dir+' '+d+' OFC'
             os.system(command_string)
 
-# create weights
-for d in dirnames:
-        #skip bias and flat images
-        if d.find('BIAS') > -1:
-            continue
-        if d.find('FLAT') > -1:
-            continue
-        if os.path.exists(d):
-            command_string = theli_path+'transform_ds9_reg.sh '+working_dir+' '+d
-            os.system(command_string)
-            command_string = theli_path+'parallel_manager.sh create_weights_para.sh '+working_dir+' '+d+' OFC'
-            os.system(command_string)
+# create astrometric reference catalog
+'''
+This calls scripts/create_astrorefcat_fromWEB.sh
+
+which then calls sqlcl.py - so need to make sure that python is python2
 
 
+
+'''
+def create_astroref_cat():
+    pass
+
+
+if __name__ == '__main__':
+    print('good luck!')
+    
+    split_files()
+    #process_bias()
+    #process_flats()
+    #calibrate_images()
+    #preview_weights()
+    #create_weights()
