739c7e34dc611169b666234defa61f5d0cfea29b
rfinn <rfinn>
2020-07-08 22:56:25 -0400

    HI def plot

--- programs/vfplots.py
+++ programs/vfplots.py
@@ -10,11 +10,19 @@ GOAL:
 
 from matplotlib import pyplot as plt
 import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
 import os
 homedir = os.getenv("HOME")
 os.sys.path.append(homedir+'/github/Virgo/programs/')
 from readtables import *
 plotdir = homedir+'/research/Virgo/plots/'
+
+### FROM NED
+virgo_ra,virgo_dec = 187.697083, 12.336944
+virgo_vr = 1079.25
+
+
 class vfplots(readfulltables):
     def plotpositions(self):
         '''plot DEC vs RA, color coded by recession velocity.'''
@@ -47,9 +55,9 @@ class vfplots(readfulltables):
                 plt.ylim(plt.xlim())
     def HIdef_env(self):
         '''plot HI def for filament, field, cluster'''
-        rmax = 2
-        virgo_flag = (np.abs(self.env['distSGY_Virgo']) < rmax) & (np.abs(self.env['distSGZ_Virgo']) < rmax) & (np.abs(self.env['distSGX_Virgo']) < rmax)
-        #virgo_flag = (self.env['distSGY_Virgo']**2 + self.env['distSGZ_Virgo']**2+ self.env['distSGX_Virgo']**2) < rmax**2
+        rmax = 3.6
+        #virgo_flag = (np.abs(self.env['distSGY_Virgo']) < rmax) & (np.abs(self.env['distSGZ_Virgo']) < rmax) & (np.abs(self.env['distSGX_Virgo']) < rmax)
+        virgo_flag = (self.env['distSGY_Virgo']**2 + self.env['distSGZ_Virgo']**2+ self.env['distSGX_Virgo']**2) < rmax**2
         print('number of cluster members = ',sum(virgo_flag))
         plt.figure(figsize=(12,8))
         plt.subplots_adjust(left=.05,right=.95,hspace=.35)
@@ -69,15 +77,25 @@ class vfplots(readfulltables):
             plt.scatter(self.main['RA'][flag],self.main['DEC'][flag],c=self.a100['HIdef_bos'][flag],s=3,vmin=-.5,vmax=.5)
             if i < len(filaments):
                 if filaments[i] == '---':
-                    plt.title('Field')
+                    mytitle = 'Field'
+                    plt.title(mytitle)
                 else:
-                    plt.title(filaments[i])
+                    mytitle=filaments[i]
+                    plt.title(mytitle)
             else:
-                plt.title('Virgo')
+                mytitle = 'Virgo'
+                plt.title(mytitle)
             plt.axis([100,280,-5,40])
             ax = plt.gca()
             allax.append(ax)
             ax.invert_xaxis()
+
+            # print fraction of HI def galaxies
+            a = np.sum(self.a100['HIdef_bos'][flag] > 0.3)
+            b = np.sum(flag)
+            s = mytitle+': %.2f (%i/%i)'%(a/b,a,b)
+            print(s)
+            plt.title(s,fontsize=10)
             #if i%4 != 0:
             #    plt.yticks(())
             #if i < 8:
@@ -86,7 +104,26 @@ class vfplots(readfulltables):
         cb=plt.colorbar(ax=allax,fraction=.08)
         cb.set_label('HI def')
         plt.savefig(plotdir+'HIdef-filaments.pdf')
-        plt.savefig(plotdir+'HIdef-filaments.png')        
+        plt.savefig(plotdir+'HIdef-filaments.png')
+    def virgo_phasespace(self):
+        '''show phasespace diagram relative to center of virgo'''
+        dr = np.sqrt((virgo_ra - self.main['RA'])**2 \
+                     + (virgo_dec - self.main['DEC'])**2)
+
+        dv = self.main['vr'] - virgo_vr
+
+        plt.figure()
+        plt.plot(dr,dv,'k.',alpha=.1)
+        rmax = 3.6
+        #virgo_flag = (np.abs(self.env['distSGY_Virgo']) < rmax) & (np.abs(self.env['distSGZ_Virgo']) < rmax) & (np.abs(self.env['distSGX_Virgo']) < rmax)
+        virgo_flag = (self.env['distSGY_Virgo']**2 + self.env['distSGZ_Virgo']**2+ self.env['distSGX_Virgo']**2) < rmax**2
+        plt.axhline(y=0)        
+        plt.plot(dr[virgo_flag],dv[virgo_flag],'bo',alpha=.3)
+
+
+
+
+        
 class coplots(readCOtables):
     def plotpositions(self):
         plt.figure(figsize=(12,8))
