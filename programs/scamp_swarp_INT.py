ba3fcbef10082f67eb89a6b978b440bac8fd4352
rfinn <rfinn>
2021-01-26 18:09:12 -0500

    updates for INT Halpha data

--- programs/scamp_swarp_INT.py
+++ programs/scamp_swarp_INT.py
@@ -7,6 +7,10 @@ run scamp and swarp
 
 This assumes that the files are already sorted by pointing, but not by filter.
 
+Run this from, e.g. /home/rfinn/data/reduced/scratch-int-feb2019
+- this directory has a subdirectory for each pointing
+- the subdirectory contains both the r and Halpha image
+
 '''
 
 import os
@@ -32,7 +36,8 @@ working_dir = os.getcwd()
 # overwrite output files if they exist
 overwrite = True
 flist1.sort()
-
+runscamp=True
+runswarp=True
 for subdir in flist1: # loop through list
     if os.path.isdir(subdir) & (subdir.find('pointing') > -1):
         print('##########################################')
@@ -44,20 +49,21 @@ for subdir in flist1: # loop through list
         # move to subdirectory
         os.chdir(subdir)
         # run scamp
-        scampflag=False
-        try:
-            os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --scamp --int --filestring WFC')
-            scampflag=True
-            os.system('ls WFC.r*PA.fits > '+subdir+'_r')
-            os.system('ls WFC.Halpha*PA.fits > '+subdir+'_Halpha')
-            os.system('ls WFC.Ha6657*PA.fits > '+subdir+'_Ha6657')            
-        except:
-            print('##########################################')
-            print('WARNING: problem running scamp for ',subdir)
-            print('##########################################')
-            
+        if runscamp:
+            scampflag=False # keeps track if scamp finishes successfully
+            try:
+                os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --scamp --int --filestring WFC')
+                scampflag=True
+                os.system('ls WFC.r*PA.fits > '+subdir+'_r')
+                os.system('ls WFC.Halpha*PA.fits > '+subdir+'_Halpha')
+                os.system('ls WFC.Ha6657*PA.fits > '+subdir+'_Ha6657')            
+            except:
+                print('##########################################')
+                print('WARNING: problem running scamp for ',subdir)
+                print('##########################################')
+                
         # run swarp
-        if scampflag:
+        if runswarp:
             # count lines r band file, run if more than 2 lines
             suffix = ['_r','_Halpha','_Ha6657']
             filelists = [subdir+i for i in suffix]
@@ -73,11 +79,20 @@ for subdir in flist1: # loop through list
                     nlines = count_lines(filelists[i])
                     if nlines > 3:
                         os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+filelists[i]+' --refimage '+refimage)
+                        os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+filelists[i]+' --refimage '+refimage+' --noback')
+                # run swarp again on the rband data, using the same refimage
+                os.system('cp '+refimage+' refimage.fits')
+                os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+filelists[0]+' --refimage refimage.fits')
+                os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+filelists[0]+' --refimage refimage.fits --noback')
             else:
                 print('WARNING: No r-band mosaic, making remaining mosaics without alignment')
                 for f in filelists:
                     nlines = count_lines(f)
                     if nlines > 2:
-                        os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+filelists[0])
-                    
+                        os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+f)
+                        os.system('python ~/github/HalphaImaging/python3/uat_astr_mosaic.py --swarp --int --l '+f+' --noback')
+                    else:
+                        print('WARNING: not enough images to make mosaic in ',f)
         os.chdir(working_dir)
+        # just running on one directory for testing purposes
+        break
