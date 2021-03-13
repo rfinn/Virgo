a6a0fb93d9a58d533c8ff1f863211fdd9b58c658
Gregory Rudnick <grudnick@ku.edu>
2021-03-12 19:27:50 -0600

    automatically make files for both filters.  Don't offset along rows and columns

--- programs/bok_dither_make.py
+++ programs/bok_dither_make.py
@@ -5,7 +5,7 @@ from astropy import units as u
 import numpy as np
 from matplotlib import pyplot as plt
 
-def bok_dither_make(exptime,objroot,band,ra,dec,outfile,dithsize = 50,nexp=1):
+def bok_dither_make(exptime,objroot,ra,dec,dithsize = 50,nexp=1):
 
     '''Written by Gregory Rudnick 10 March 2021
     
@@ -14,21 +14,19 @@ def bok_dither_make(exptime,objroot,band,ra,dec,outfile,dithsize = 50,nexp=1):
     Generate a list of dithers for Bok telescope observations.  This
     will use a 5 point dither pattern
 
+    Current version hardcodes output for two filters
+
     INPUT PARAMETERS:
 
     exptime: the exposure time in seconds
 
     objroot: the root of the object name.  This will have a dither identifier appended
 
-    band:  'r' or 'Ha+4nm'
-
     ra,dec: The start of the pointing in decimal units
 
     dithsize: the dimensions of the side of the box containing the dithers (arcsec).
 
     nexp: the number of exposures at each dither position
-
-    outfile: The name of the script written by the code
 
     OUTPUT
 
@@ -39,54 +37,72 @@ def bok_dither_make(exptime,objroot,band,ra,dec,outfile,dithsize = 50,nexp=1):
     EXAMPLE CALLING SEQUENCE
 
     import bok_dither_make as bkd
-    bkd.bok_dither_make(240.0,'test','r',23.456,9.001,'test.out')
+    bkd.bok_dither_make(240.0,'test',23.456,9.001)
+
+    TODO:
+
+    build possibility to repeat dither pattern with offsets in center between dither patterns
 
     '''
 
     #dither offsets relative to *initial* pointing
+    #include small additions to some offset points to make sure that we aren't
+    #offsetting directly along rows and column
+    dithpad = 5.0
     radith = np.array([0.0,
                            dithsize/2.0,
                            -dithsize/2.0,
-                           -dithsize/2.0,
-                           dithsize/2.0])
+                           -dithsize/2.0 - dithpad,
+                           dithsize/2.0 + dithpad])
 
     decdith = np.array([0.0,
                             dithsize/2.0,
-                            dithsize/2.0,
+                            dithsize/2.0 + dithpad,
                            -dithsize/2.0,
-                           -dithsize/2.0])
+                           -dithsize/2.0 - dithpad])
 
     #convert to degrees
     radith /= 3600.0
     decdith /= 3600.0
+
+    #convert dec to radians
+    decrad = dec * np.pi / 180.
     
     #apply dither
-    radithdeg = ra + radith
+    radithdeg = ra + radith /  np.cos(decrad)   #account for cos(dec) term in ra offset
     decdithdeg = dec + decdith
 
     #convert to sexigesimal
     RAdithhms = Angle(radithdeg,unit='deg').to_string(unit=u.hour,sep='')
     DECdithhms = Angle(decdithdeg,unit='deg').to_string(unit=u.degree,sep='')
 
-    #open output file
-    fo = open(outfile, "w")
+    #output prefix
+    outfile_pre = "dither_" + objroot + "_t" + str(exptime) + "_nexp" + str(nexp) + "_dsize" + str(dithsize)
 
-    for idith,val in enumerate(radith):
-        dithname = objroot + '_dith' + str(idith)
+    filts = ["r", "Ha+4nm"]
+    #output files
+    for filt in filts:
+        outfile = outfile_pre + "_filt_" + filt + ".txt"
+        fo = open(outfile, "w")
 
-        #make strings for positions
-        rastr = "{:0>9.2f}".format(float(RAdithhms[idith]))
-        #DECdithhms[idith]
-        if float(DECdithhms[idith]) < 0:
-            decstr = "-{:0>8.1f}".format(abs(float(DECdithhms[idith])))
-        else:
-            decstr = "+{:0>8.1f}".format(float(DECdithhms[idith]))
+        #compute dithers, assumed to be the same for each filter
+        for idith,val in enumerate(radith):
+            dithname = objroot + '_filt_' + filt + '_dith' + str(idith)
+
+            #make strings for positions
+            rastr = "{:0>9.2f}".format(float(RAdithhms[idith]))
+            #DECdithhms[idith]
+            if float(DECdithhms[idith]) < 0:
+                decstr = "-{:0>8.1f}".format(abs(float(DECdithhms[idith])))
+            else:
+                decstr = "+{:0>8.1f}".format(float(DECdithhms[idith]))
             
-        print(rastr,decstr)
-        #print(idith,RAdithhms[idith],DECdithhms[idith])
-        fo.write('obs {} object {} {} {} {} {} 2000.0\n'.format(exptime,dithname,nexp,band,rastr,decstr))
-
-    fo.close()
+            print(rastr,decstr)
+            #print(idith,RAdithhms[idith],DECdithhms[idith])
+            #write to each filter
+            fo.write('obs {} object {} {} {} {} {} 2000.0\n'.format(exptime,dithname,nexp,filt,rastr,decstr))
+       
+        fo.close()
 
 
     
