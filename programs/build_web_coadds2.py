5b4760dd19b3104a73d54e70d3b69ec3967fcca3
rfinn <rfinn@siena.edu>
2023-06-09 13:07:51 -0400

    updates

--- programs/build_web_coadds2.py
+++ programs/build_web_coadds2.py
@@ -1087,7 +1087,7 @@ if __name__ == '__main__':
     
     indices = np.arange(len(rfiles))
     image_pool = mp.Pool(mp.cpu_count())
-    myresults = [image_pool.apply_async(buildone,args=(rimages,i,coadd_dir,psfdir,zpdir,fratiodir),callback=collect_results) for i in indices]
+    myresults = [image_pool.apply_async(buildone,args=(rimages,i,coadd_dir,psfdir,zpdir,fratiodir),callback=collect_results) for i in indices[0:3]]
     
     image_pool.close()
     image_pool.join()
