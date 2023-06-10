2b401f51ac2c17f317a4d8014e6c20a104211f61
rfinn <rfinn>
2023-06-10 15:06:46 -0400

    updates

--- programs/build_web_coadds2.py
+++ programs/build_web_coadds2.py
@@ -656,7 +656,7 @@ class pointing():
         for j in range(len(galra)):
             #print(sizes[j][0])
             try:
-                imsize = galsizes[j]/pixelscale*3.
+                imsize = galsizes[j]/pixscale*3.
             except IndexError:
                 print('hey rose - problem accessing sizes ',sizes)
                 # set the default size to 90 arcsec
