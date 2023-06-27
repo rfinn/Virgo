70e4c8b860da5b9e106cd57f5a9a0c0ff111e9db
rfinn <rfinn>
2023-06-27 15:56:22 -0400

    typos and bugs - fixes #9

--- programs/build_web_coadds2.py
+++ programs/build_web_coadds2.py
@@ -124,12 +124,12 @@ def buildone(rimages,i,coadd_dir,psfdir,zpdir,fratiodir):
     # define previous gal for html links
     if i > 0:
         previous = os.path.basename(rfiles[i-1]).replace('-R.fits','').replace('-shifted','').replace('-r.fits','').replace('.fits','').replace('-R','').replace('-r','')
-        print('previous = ',previous)
+        #print('previous = ',previous)
     else:
         previous = None
     if i < len(rfiles)-1:
         next = os.path.basename(rfiles[i+1]).replace('-R.fits','').replace('-shifted','').replace('-r.fits','').replace('.fits','').replace('-R','').replace('-r','')
-        print('next = ',next)
+        #print('next = ',next)
     else:
         next = None
     # define pointing name - remove fits and filter information
