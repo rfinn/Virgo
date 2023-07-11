5b501bda7e2183226f88ce7094390370a619758e
rfinn <rfinn>
2023-07-11 15:04:44 -0400

    updates

--- programs/build_web_cutouts2.py
+++ programs/build_web_cutouts2.py
@@ -149,7 +149,7 @@ def display_image(image,percentile1=.5,percentile2=99.5,stretch='asinh',mask=Non
     else:
         norm = simple_norm(clipped_data, stretch=stretch,max_percent=percentile2)
 
-    plt.imshow(image, norm=norm,cmap='gray_r',origin='lower',vmin=v1,vmax=v2)
+    plt.imshow(image, norm=norm,cmap='gray_r',origin='lower')#,vmin=v1,vmax=v2)
     
 
 def make_png(fitsimage,outname):
