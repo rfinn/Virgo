3ce719dd4801a5ba468319639c88a8f1e63f30a2
rfinn <rfinn>
2024-05-22 10:06:32 -0400

    script to compare image sizes of BOK halpha and shifted r-band images

---
+++ programs/BOK_check_coadd_dimensions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from astropy.io import fits
+import glob
+
+
+rfiles = glob.glob("*r-shifted.fits")
+
+for r in rfiles:
+    rheader = fits.getheader(r)
+    rdata = fits.getdata(r)
+    himage = rheader['HAIMAGE']
+    rnaxis1,rnaxis2 = rdata.shape()
+
+
+    # get dimensions of halpha image
+    hdata = fits.getdata(himage)
+
+    hnaxis1,hnaxis2 = hdata.shape()    
+
+    if (rnaxis1 == hnaxis1) & (rnaxis2 == hnaxis2):
+        continue
+    else:
+        print("HOLD UP: ",r)
+        print(f"\tr-band dimensions = {rnaxis1},{rnaxis2}"
+        print(f"\thalpha dimensions = {hnaxis1},{hnaxis2}"              
