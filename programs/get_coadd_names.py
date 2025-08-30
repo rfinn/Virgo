8010f44fcf5f78b825fdfea7156b0a304f518f04
rfinn <rfinn@siena.edu>
2025-08-30 10:18:24 -0400

    - creating lists with halpha fits files for running batch_getzp.py in parallel on halpha as well as r files

--- programs/get_coadd_names.py
+++ programs/get_coadd_names.py
@@ -57,3 +57,50 @@ outfile.close()
 outfile2.close()
 outfile3.close()
 outfile4.close()
+
+
+
+######################################################
+# REPEAT FOR THE HALPHA IMAGES
+######################################################
+a = glob.glob('VF*INT*-Halpha.fits')
+b = glob.glob('VF*INT*-Ha6657.fits')
+#b = glob.glob('VF*HDI*-r.fits')
+c = glob.glob('VF*HDI*-ha4.fits')
+
+#d = glob.glob('VF*BOK*-r.fits')
+#######################################################
+# updating to use the shifted r-band images
+#######################################################    
+d = glob.glob('VF*BOK*-Ha4.fits')
+
+e = glob.glob('VF*MOS*-Ha4.fits')         
+hfiles = a + b + c + d + e
+
+if args.bokonly:
+    hfiles = d
+hfiles.sort()
+print(f"number of targets = {len(hfiles)}")
+
+# write out as a csv file
+outfile = open('virgo-coadds-ha.csv','w')
+outfile4 = open('virgo-coadds-ha-test.csv','w')
+outfile2 = open('virgo-coadds-ha-fullpath.txt','w')
+outfile3 = open('virgo-coadds-ha-fullpath-test.txt','w')
+if args.bokonly:
+    outfile = open('virgo-bok-coadds-ha.csv','w')
+    outfile2 = open('virgo-bok-coadds-ha-fullpath.txt','w')
+    outfile3 = open('virgo-bok-coadds-ha-fullpath-test.txt','w')
+    outfile4 = open('virgo-bok-coadds-ha-test.csv','w')    
+coadd_dir = '/data-pool/Halpha/coadds/all-virgo-coadds'
+for i in range(len(hfiles)):
+    #basname = rfiles[i].replace("-r-shifted.fits","").replace("-r.fits","").replace("-R.fits","")
+    outfile.write(f"{hfiles[i]}\n")
+    outfile2.write(f"{coadd_dir}/{hfiles[i]}\n")
+    if i < 2:
+        outfile3.write(f"{coadd_dir}/{hfiles[i]}\n")
+        outfile4.write(f"{hfiles[i]}\n")
+outfile.close()
+outfile2.close()
+outfile3.close()
+outfile4.close()
